f5e975592d72bd9855e6ab10ac58955427cfeab9
snapcrack <arvcondor@gmail.com>
2019-01-24 02:00:16 -0500

    typo

--- js/ngram-main.js
+++ js/ngram-main.js
@@ -6,7 +6,7 @@ function initPhrasesAll (container) {
 
     let data = null;
     
-    d3.csv('./data/ngram_df_flattened.csv')
+    d3.csv('./data/ngram_df_all_flattened.csv')
      .then(response => {
         data = response;
         chart.innerHTML = ''

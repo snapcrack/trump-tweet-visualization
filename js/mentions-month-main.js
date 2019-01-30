8fd16410eed6621882dcb6c4c730c13d4e872273
Giorgi Ghviniashvili <mr.g.ghv@gmail.com>
2019-01-30 11:24:03 +0400

    dropbox fix

--- js/mentions-month-main.js
+++ js/mentions-month-main.js
@@ -13,7 +13,7 @@ function initMentionsMonth (mode, _container) {
   
     // loading data from google sheets
     // loadSheet(sheetUrl)
-    d3.csv('https://www.dropbox.com/s/1iilit946sk91uc/mention_df.csv?dl=1')
+    d3.csv('https://dl.dropboxusercontent.com/s/1iilit946sk91uc/mention_df.csv?dl=1')
       .then(response => {
         // var data = {
         //   mentions: response['mention_df'].elements
